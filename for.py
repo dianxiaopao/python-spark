28526e2f951036fcd8f23cb2e9669084ba01faa5
zhangbin <zhangbin@misrobot.com>
2017-09-06 18:35:03 +0800

    demo2

---
+++ for.py
@@ -0,0 +1,22 @@
+from pyspark.shell import spark
+from pyspark.sql import Row
+
+sc = spark.sparkContext
+
+# Load a text file and convert each line to a Row.
+lines = sc.textFile("examples/src/main/resources/people.txt")
+parts = lines.map(lambda l: l.split(","))
+people = parts.map(lambda p: Row(name=p[0], age=int(p[1])))
+
+# Infer the schema, and register the DataFrame as a table.
+schemaPeople = spark.createDataFrame(people)
+schemaPeople.createOrReplaceTempView("people")
+
+# SQL can be run over DataFrames that have been registered as a table.
+teenagers = spark.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
+
+# The results of SQL queries are Dataframe objects.
+# rdd returns the content as an :class:`pyspark.RDD` of :class:`Row`.
+teenNames = teenagers.rdd.map(lambda p: "Name: " + p.name).collect()
+for name in teenNames:
+    print(name)
